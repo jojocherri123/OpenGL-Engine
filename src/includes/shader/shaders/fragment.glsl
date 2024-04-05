62535f6ac9019f6f032cdc8c5084dced0a6e1049
jojocherri123 <107985822+jojocherri123@users.noreply.github.com>
2024-04-05 12:17:44 +0300

    Update fragment.glsl

--- src/includes/shader/shaders/fragment.glsl
+++ src/includes/shader/shaders/fragment.glsl
@@ -4,11 +4,34 @@
 out vec4 color;
 
 in vec2 texCoords;
+in vec3 normals;
+in vec3 crntPos;
 
 uniform sampler2D tex;
+uniform vec4 u_LightColor;
+uniform vec3 u_LightPosition;
+uniform vec3 u_CamPos;
 
 void main(){
 
-    color = texture(tex,texCoords);
+    float ambient = 0.02f;
+
+    vec3 normal = normalize(normals);
+	vec3 lightDirection = normalize(u_LightPosition - crntPos);
+	float diffuse = max(dot(normal, lightDirection), 0.0f);
+
+    float specular = 0.0f;
+    
+    if (diffuse !=0.0f){
+        float specularLight = 0.5f;
+        vec3 viewDirection = normalize(u_CamPos-crntPos);
+        vec3 reflectionDirection = reflect(-lightDirection, normal);
+        vec3 halfwayVec = normalize(viewDirection +lightDirection);
+        float specularAmount = pow(max(dot(normal, halfwayVec), 0.0f), 8);
+        specular = specularAmount * specularLight;
+
+    };
+
+    color = texture(tex,texCoords) *u_LightColor *(diffuse + ambient + specular);
 
 }
