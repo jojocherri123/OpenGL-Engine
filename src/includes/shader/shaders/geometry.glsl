54ce492f241a63927c0507d5c9d2af99615afaa7
jojocherri <107985822+jojocherri123@users.noreply.github.com>
2024-04-09 19:00:17 +0300

    specular maps

---
+++ src/includes/shader/shaders/geometry.glsl
@@ -0,0 +1,41 @@
+#version 460 core
+
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+
+out vec3 normals;
+out vec3 crntPos;
+out vec2 texCoord;
+
+in DATA
+{
+    vec3 normals;
+	vec3 crntPos;
+    vec2 texCoords;
+    mat4 projection;
+} data_in[];
+
+
+// Default main function
+void main()
+{
+    gl_Position = data_in[0].projection * gl_in[0].gl_Position;
+    normals = data_in[0].normals;
+    crntPos = data_in[0].crntPos;
+    texCoords = data_in[0].texCoords;
+    EmitVertex();
+
+    gl_Position = data_in[1].projection * gl_in[1].gl_Position;
+    normals = data_in[1].normals;
+    crntPos = data_in[1].crntPos;
+    texCoords = data_in[1].texCoords;
+    EmitVertex();
+
+    gl_Position = data_in[2].projection * gl_in[2].gl_Position;
+    normals = data_in[2].normals;
+    crntPos = data_in[2].crntPos;
+    texCoords = data_in[2].texCoords;
+    EmitVertex();
+
+    EndPrimitive();
+}
